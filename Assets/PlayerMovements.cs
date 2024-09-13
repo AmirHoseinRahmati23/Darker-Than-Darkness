73516c5d82471b8c90ba602919e9efa4d308e97a
AmirHoseinRahmati23 <www.amir233@gmail.com>
2024-09-13 20:21:23 +0330

    Camera Follow Player

--- Assets/PlayerMovements.cs
+++ Assets/PlayerMovements.cs
@@ -8,7 +8,7 @@ public class PlayerMovements : MonoBehaviour
     private PlayerControls controls;
     private Rigidbody2D rigidbody2d;
     private Animator animator;
-
+    private Transform cameraTransform;
     [Header("Movement Info")]
     [SerializeField] private Vector2 moveInfo;
     [SerializeField] private float moveSpeed = 2.5f;
@@ -54,15 +54,25 @@ public class PlayerMovements : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         controls = player.controls;
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
     
     private void ApplyMovement()
     {
         if (moveInfo.magnitude > 0)
         {
+            // Character Move in X direction
             var targetPosition =
                 new Vector2(transform.position.x + moveInfo.x, transform.position.y);
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            // Camera Move with the player
+
+            var cameraTargetX = (targetPosition.x < 0.57f) ? 0.57f : targetPosition.x;
+            var cameraTargetPosition = new Vector3(cameraTargetX, cameraTransform.position.y, cameraTransform.position.z);
+            cameraTransform.position = Vector3.MoveTowards(cameraTransform.position, cameraTargetPosition, moveSpeed * Time.deltaTime);
+            
+            // Up and Down Movement(When Near The Rope)
             if (canGoUp && moveInfo.y != 0)
             {
                 rigidbody2d.gravityScale = 0;
